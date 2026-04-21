4ec92b445728d49505bd9e9a24452215f3f89cbb
darknesabc <161279889+darknesabc@users.noreply.github.com>
2026-04-21 20:12:09 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -1534,8 +1534,7 @@ window.__openUnivSimulation = async function() {
                 return a.localeCompare(b); 
             });
 
-            return { matches, sortedUnivs };
-        };
+        
 
         window.runUniversitySimulation = function() {
             const st = window.__currentSimStatus;
